59654a93412b01be8948d2d7a5b379c558c84c96
Ian J Sikes <ianjsikes@gmail.com>
2016-11-12 14:07:27 -0800

    Experimenting with layout of content pages

--- src/components/about.js
+++ src/components/about.js
@@ -5,15 +5,7 @@ export default class About extends Component {
   render() {
     return (
       <div className='about'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id gravida sem, vel ornare nulla. Curabitur quam diam, pellentesque in iaculis et, pulvinar at lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id gravida sem, vel ornare nulla. Curabitur quam diam, pellentesque in iaculis et, pulvinar at lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id gravida sem, vel ornare nulla. Curabitur quam diam, pellentesque in iaculis et, pulvinar at lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
+        
       </div>
     );
   }
--- src/components/team.js
+++ src/components/team.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
+import '../css/team.css';
+import placeholder from '../img/placeholder.png';
+
+
+const Officer = ({ name, title, pic }) => (
+  <div className='officer'>
+    <img className='pic' src={ pic || placeholder } alt={ name || 'Placeholder' } />
+    <h3 className='name'>{ name || 'Place Holder' }</h3>
+    <p className='title'>{ title || 'Holder of Places' }</p>
+  </div>
+);
 
 export default class Team extends Component {
   render() {
+    let officers = []; // Test array
+    _.times(6, i => officers.push(<Officer key={i} />));
+
     return (
-      <div><h3>Team</h3></div>
+      <div className='team'>{ officers }</div>
     );
   }
 }
